ec318afd8d55eca2d32923f878d4c8c85770f7c2
Connecting69 <austinli823@gmail.com>
2021-09-10 20:41:03 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const initialData = [
+  {
+    title: 'Winnie',
+    color: 'rgb(142, 110, 149)',
+    todos: ['Do homework', 'Do laundry'],
+  },
+  {
+    title: 'Bob',
+    color: 'rgb(57, 165, 156)',
+    todos: ['Play Video Game', 'Level Up'],
+  },
+  {
+    title: 'Thomas',
+    color: 'rgb(52, 71, 89)',
+    todos: ['Get a girlfriend', 'Go to school'],
+  },
+  {
+    title: 'George',
+    color: 'rgb(232, 116, 30)',
+    todos: ['Cheat on exam', 'Lie to parents'],
+  },
+];
 
 function App() {
+  const [data, setData] = React.useState(initialData);
+
+  const addTodo = (index) => {
+    const newTodo = prompt('What do you want to do?')
+    const nextData = [...data]
+    nextData[index].todos.push(newTodo)
+    setData(nextData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {data.map((v, i) => {
+        return (
+          <div className='column' key={i}>
+            <div className='header' style={{ backgroundColor : v.color }}>
+              <span>{v.title}</span>
+            </div>
+            <div className='list'>
+              {v.todos.map((w, j) => {
+                return (
+                  <div key='j' className='todo'>
+                    <button className={i === 0 && 'hidden'}>Prev</button>
+                    <span>{w}</span>
+                    <button className={i === initialData.length - 1 && 'hidden'}>Next</button>
+                  </div>
+                )
+              }
+
+              )}
+            </div>
+            <button onClick={() => addTodo(i)}>+ Add new todo</button>
+          </div>
+        );
+      })}
     </div>
   );
-}
-
+    };
+  
+  
 export default App;
+
